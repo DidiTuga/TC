b657373fa2b16ff6ce64dadf68a13ada1a9bc793
DidiTuga <diogo.diogosnts.santos2@gmail.com>
2022-12-30 15:59:32 +0000

    Trabalho B finalizado!!!

---
+++ Problema B/README.md
@@ -0,0 +1,43 @@
+
+# Problema B - Teoria da Computação 2022/2023
+
+
+## Algoritmo CYK (de Cocke, Younger e Kasami)
+
+## Input
+Na primeira linha é introduzida a palavra por reconhecer. Esta palavra só é constituída por caracteres do alfabeto Σ={a,…, z} e tem por comprimento máximo 50 caracteres. Assume-se que o conjunto de não-terminais é N={A,B, … S , …, Z} em que se destaca o símbolo S que assumiremos como sendo sempre o símbolo inicial.
+
+A segunda linha apresenta o número m de regras de produções da gramática considerada. As restantes m linhas introduzem as m regras de produção. Cada uma destas produções tem o formato seguinte N -> a1 a2 a3 … an com N não terminal e ai ∈ (N ∪ Σ) e cada ai é separado do aj seguinte por um espaço único.
+
+## Output
+O output é constituído por uma linha contendo:
+
+A palavra "YES" se a palavra é gerada pela gramática.
+A palavra "‘NO" se a palavra não é gerada (reconhecida) pela gramática
+Seguida da matriz triangular inferior, usada para reconhecer (ou não) a palavra.
+```
+NOTAS:
+
+cada célula da matriz é separada da célula adjacente à direita por 2 tabs (\t\t).
+Sempre que uma célula contém mais do que um não terminal, estes são separados por um só espaço, e estes estão ordenados por ordem alfabética.
+```
+## Sample Input 1
+abbabba
+7
+S -> S F
+S -> a
+A -> C C
+A -> S S
+A -> C S
+C -> b
+F -> A S
+## Sample Output 1
+YES
+S               
+  F             
+    A           
+S       S         
+  F       F       
+  A   A     A   A     
+S   C   C   S   C   C   S   
+a   b   b   a   b   b   a   
--- Problema B/probB.ml
+++ Problema B/probB.ml
@@ -1,12 +1,14 @@
-(*
-Autores: 
+(*  Algoritmo CYK (de Cocke, Younger e Kasami)
+  Referências: 
+  - Grupo do Teams, Enunciado do Trabalho.
+
+  30/12/2022
+
+  Autores: 
   30646 - Luis Santos
   45842 - Diogo Santos
-  
 *)
-
-(*Algoritmo CYK (de Cocke, Younger e Kasami)*)
-(*Input *)
+open List 
 
 (*Ler palavra para reconhecer *)
 let palavra = read_line ()
@@ -14,106 +16,114 @@ let palavra = read_line ()
 (*Ler quantas transições temos*)
 let num_transicoes = read_int ()
 
-(*Função para ler uma transição e devolve os caracteres, o primeiro é de onde vem para onde vai S -> a = (S,a)*)
+(* Sabendo que são no maximo 3 caracteres, a função vai ler a linha e devolver uma lista com os caracteres*)
 let lerLinha = 
   let list = ref [] in
   for i = 1 to num_transicoes do
     let linha = read_line () in
-    if String.length linha == 6 then list:= !list @ [(linha.[0], linha.[5], ' ')]
-    else list:= !list @ [(linha.[0], linha.[5], linha.[7])]
+    let linha = String.split_on_char '>' linha in
+    let linha = String.concat "" linha in
+    let linha = String.split_on_char '-' linha in
+    let linha = String.concat "" linha in
+    let linha = String.split_on_char ' ' linha in
+    let linha = String.concat "" linha in
+    if String.length linha < 3 then list:= !list @ [((String.make 1 linha.[0]), (String.make 1 linha.[1]), " ")]
+    else list:= !list @ [((String.make 1 linha.[0]), (String.make 1 linha.[1]), (String.make 1 linha.[2]))]
   done;
   !list
-
-
+  
 (*Lista onde vai guardar todas as transiçoes*)
 let list_trans = lerLinha
 
-
-let explode_string s = List.init (String.length s) (String.get s)
-
-
-
-(*Função para verificar se uma letra está na lista*)
-
-(*cyk é a matriz que vai guardar os resultados*)
+(*Função que vai percorrer a matriz e verificar se a palavra pertence a L(G) *)
 let cyk = 
-  (*
-  DEBUG
-  List.iter (fun (a,b,c) -> print_endline (String .make 1 a ^ String .make 1 b ^ String .make 1 c )) list_trans;
-  *)
   (*tamanho da palavra*)
   let m = String.length palavra in
   (*x y valor para inicial*)
   let matrix = Array.make_matrix (m) (m+1) [] in
   (*inicializar com a ultima camada a 0*)
   for i = 0 to m-1 do
-    matrix.(i).(m) <- [palavra.[i]]
+    matrix.(i).(m) <- [String.make 1 palavra.[i]]
   done;
-  (*Preencher a matriz*)
+  (*Variavel que vai guardar as novas transições*)
   let novas_transicoes = ref [] in
-  for j = m-1 downto 0 do
-    for i = 0 to j do
-      (*numero de caracteres a ir buscar *)
+  for j = m-1 downto 0 do (*j é colunas y*)
+    for i = 0 to j do (*i é as linhas x*)
       let num = m - j in
       let string = String.sub palavra i num in
-      let list_string = explode_string string in
-      (*print_endline string;*)
-      (*verificar se a string pertence a L(G)*)
-      (*Ir percorrer caracter a caracter*)
-      if List.length list_string == 1 
-      then(
+      (*Para um caracater vai verificar se está na lista e se estiver adiciona na matriz e nas novas transições*)
+      if String.length string = 1 then(
         (*Verificar se o caracter está na lista*)
-        let list = List.find_all (fun (a,b,c) -> b = List.hd list_string || c = List.hd list_string) list_trans in
-        (*Se estiver, adicionar ao array*)
-        if list <> [] then
-          matrix.(i).(j) <- matrix.(i).(j) @ (List.map (fun (a,b,c) -> a) list);
-          novas_transicoes := !novas_transicoes @ List.map (fun (a,b,c) -> (String.make 1 a, String.make 1 b, String.make 1 c)) list
+        let list = find_all (fun (a,b,c) -> b = string || c = string) list_trans in
+        (*Se estiver, adicionar a matriz*)
+        if length list >0 then
+          matrix.(i).(j) <- matrix.(i).(j) @ (map (fun (a,b,c) -> a) list);
+          novas_transicoes := !novas_transicoes @ map (fun (a,b,c) -> (a, b, c)) list
       )else(
-        (*Percorrer a string exemplo aa fica a,a ; aba -> a,ba e ab,a*)
+        (*Percorrer a string exemplo: aba -> a,ba e ab,a*)
         for k = 0 to num-2 do
           let cor_string_1 = String.sub string 0 (k+1) in
           let cor_string_2 = String.sub string (k+1) (num-k-1) in
-          (*DEBUG
-          print_endline ("cor_1" ^ cor_string_1);
-          print_endline ("cor_2" ^ cor_string_2);
-          *)
-          let list = List.map (fun (a,b,c) -> (a)) (List.find_all (fun (a,b,c) -> b = cor_string_1 || c = cor_string_1) !novas_transicoes) in 
-          let list_2 = List.map (fun (a,b,c) -> (a)) (List.find_all (fun (a,b,c) -> b = cor_string_2 || c = cor_string_2) !novas_transicoes) in 
-          if list <> [] && list_2 <> [] then
-            (*junta os a das listas *)
-            let nova_lista = List.concat(List.map (fun x -> List.map (fun y -> (x,y)) list_2) list) in
+          (*Verificar se as 2 string estão na lista*)
+          let list_1 = map (fun (a,b,c) -> (a)) (find_all (fun (a,b,c) -> b = cor_string_1 || c = cor_string_1) !novas_transicoes) in 
+          let list_2 = map (fun (a,b,c) -> (a)) (find_all (fun (a,b,c) -> b = cor_string_2 || c = cor_string_2) !novas_transicoes) in 
+          if length list_1 > 0 && length list_2 > 0 then(
+            (*junta os "a" das listas *)
+            let nova_lista = concat(map (fun x -> map (fun y -> (x,y)) list_2) list_1) in
             (*Procurar os que tem x,y*)
-            let nova_lista = List.concat ( List.map  (fun (x,y)  ->  List.find_all( fun (a,b,c) ->  (String.make 1 b)=x && y =(String.make 1 c)) list_trans )  nova_lista) in
-            
-
+            let nova_lista = concat ( map  (fun (x,y)  ->  find_all( fun (a,b,c) ->  b=x && y =c) list_trans )  nova_lista) in
             (*Retirar os repetidos e meter por ordem alphabetica*)
-
-            let nova_lista = List.sort_uniq (fun (a,b,c) (d,e,f) -> compare a d) nova_lista in
-            (*DEBUG
-            List.iter (fun (a,b,c) -> print_endline (String.make 1 a)) nova_lista;
-            *)
-            
-          (*Se estiver, adicionar ao array*)
-            matrix.(i).(j) <- matrix.(i).(j) @ (List.map (fun (a,b,c) -> a) nova_lista);
-            novas_transicoes := !novas_transicoes @ List.map (fun (a,b,c) -> (String.make 1 a, cor_string_1^cor_string_2, "")) nova_lista
-
-          
-
+            let nova_lista = sort_uniq compare nova_lista in
+          (*adiciona na matriz*)
+            matrix.(i).(j) <- matrix.(i).(j) @ (map (fun (a,b,c) -> a) nova_lista);
+            let nova_lista = map (fun (a,b,c) -> (a, cor_string_1^cor_string_2, "")) nova_lista in
+            (* se a nova_lista nao tiver nas novas transições adiciona*)
+            if for_all (fun (a,b,c) -> not (mem (a,b,c) !novas_transicoes)) nova_lista then novas_transicoes := !novas_transicoes @ nova_lista;)
         done
       )
     done;
   done;
   (*Se exister um S na primeira posição da matriz da print a YES se não a NO*)
-  if List.mem 'S' matrix.(0).(0) then print_endline "YES" else print_endline "NO";
-  
+  if mem "S" matrix.(0).(0) then print_endline "YES" else print_endline "NO";
   (**Print da matriz que guarda os resultados em lista de chars*)
   for i = 0 to m do
     for j = 0 to m-1 do
-      Printf.printf "%s\t\t" (String.concat " " (List.map (fun x -> String.make 1 x) (List.sort_uniq (fun (a) (d) -> compare a d) matrix.(j).(i))))
+      Printf.printf "%s\t\t" (String.concat " " (map (fun x -> x) (sort (fun (a) (d) -> compare a d) (sort_uniq compare matrix.(j).(i)))))
     done;
     Printf.printf "\n"
   done;;
-  
-  
-  
 
+  (*
+  Indice:
+    1. Input
+    2. Execução do Programa
+    3. Ouput 
+    
+  1. Input
+    aabaa
+    4
+    S -> A B
+    S -> b
+    A -> a 
+    B -> S A
+  
+  2. Execução do Programa
+  palavra = aabaa
+  num_transicoes = 4
+  list_trans = [("S", "A", "B"); ("S", "b", ""); ("A", "a", " "); ("B", "S", "A")]
+  cyk =
+  S										novas_transicoes = [("A", "a", ""); ("S", "b", ""); ("B", "ba", ""); ("S", "aba", ""); ("B", "abaa", ""), ("S", "aabaa", "")]
+      B								novas_transicoes = [("A", "a", ""); ("S", "b", ""); ("B", "ba", ""); ("S", "aba", ""); ("B", "abaa", "")]
+      S								novas_transicoes = [("A", "a", ""); ("S", "b", ""); ("B", "ba", ""); ("S", "aba", "")]
+          B						novas_transicoes = [("A", "a", ""); ("S", "b", ""); ("B", "ba", "")]
+  A		A		S		A		A		novas_transicoes = [("A", "a", ""); ("S", "b", "") ]
+  a		a		b		a		a		
+  3. Output
+  YES
+  S										
+      B								
+      S								
+          B						
+  A		A		S		A		A		
+  a		a		b		a		a		
+  *)
